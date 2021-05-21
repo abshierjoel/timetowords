70cc2aaa5fdd953872cd7f28b192ecd210610975
Joel Abshier <JAbshier@midwayusa.com>
2021-05-21 10:06:30 -0500

    Refactor to reduce mutation

--- script.js
+++ script.js
@@ -1,87 +1,92 @@
-/*
-    convertTimeToWords
-        Inputs: int, int
-        Outputs: null
-        Description: This function takes two integer inputs and uses those values to output a string that is a text representation of the time.
-*/
-function convertTimeToWords( hours, minutes )
-{
+const hourField = document.getElementById('hour');
+const minuteField = document.getElementById('minute');
+const output = document.getElementById('time');
 
-    //Remove leading zero, if placed
-    if( minutes == 00 ) 
-        minutes = '0';
-        
-    //If either input is outside of the valid ranges (1-12 for hours; 0-59 for minutes) report error and stop computing
-    if( !( hours <= 12 && hours >= 1 ) || !( minutes <= 59 && minutes >= 0 ) || isNaN( hours ) || isNaN( minutes )  )
-    {
-        //Display error instead of time
-        document.getElementById('time').innerHTML = "Incomputable!";
-        
-        //Exit convertTimeToWords function
-        return;
-    }
-    
-    //Array that contains conversions for all numbers into their text counterpart
-    var textNumbers = [ "NULL", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-        "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty-one", "twenty-two", "twenty-three", "twenty-four", "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine", "thirty",
-        "thirty-one", "thirty-two", "thirty-three", "thirty-four", "thirty-five", "thirty-six", "thirty-seven", "thirty-eight", "thirty-nine", "forty", "forty-one", "forty-two", "forty-three", "forty-four", "forty-five",
-        "forty-six", "forty-seven", "forty-eight", "forty-nine", "fifty", "fifty-one", "fifty-two", "fifty-three", "fifty-four", "fifty-five", "fifty-six", "fifty-seven", "fifty-eight", "fifty-nine" ];
+const convertTimeToWords = (hours, minutes) => {
+  output.innerHTML = getTimeText(hours, minutes);
+};
 
-    //This switch case handles special cases for the hour (0, 1/4,1/2, and 3/4) and provides different text outputs/phrasing for each
-    switch( minutes )
-    {
-    
-        case '0':
-            //If the minutes are 00, output O'Clock
-            document.getElementById('time').innerHTML = `${ textNumbers[hours] } O\`Clock`;
-            break;
-        case '15':
-            //If the minutes are 15, output Quarter past current hour
-            document.getElementById('time').innerHTML = `Quarter past ${ textNumbers[hours] }`;
-            break;
-        case '30':
-            //If the minutes are 30, output Half past current hour
-            document.getElementById('time').innerHTML = `Half past ${ textNumbers[hours] }`;
-            break;
-        case '45':
-            //If the minutes are 45, output Quarter to next hour
-            document.getElementById('time').innerHTML = `Quarter to ${ textNumbers[( (hours % 12) + 1 )] }`;
-            break;
-        default:
-            //If only one minute has passed in the hour or it is the last, write the word 'minute', otherwise write the plural 'minutes'
-            adds = `${ ( ( minutes > 1 ) && ( minutes < 59 ) ) ? ' minutes' : ' minute' }`;
-            //for all other times, output the current minutes to/past the next/current hour.
-            document.getElementById('time').innerHTML = `${ minutes>30 ? ( textNumbers[(60-minutes)] + adds + ' to ' + textNumbers[( (hours % 12) + 1 )] ) : textNumbers[minutes] + adds + ' past ' + textNumbers[hours] }`;
-    
-    }  
+const getTimeText = (hours, minutes) => {
+  if (
+    !(hours <= 12 && hours >= 1) ||
+    !(minutes <= 59 && minutes >= 0) ||
+    !Number.isInteger(hours) ||
+    !Number.isInteger(minutes)
+  ) {
+    return 'Incomputable!';
+  }
 
-}
+  const hoursInTwelve = twentyFourToTwelveHour(hours);
+  const minutesWithoutZero = removeLeadingZero(minutes);
 
+  switch (minutes) {
+    case '0':
+      return `${timeWords[hoursInTwelve]} O\`Clock`;
+    case '15':
+      return `Quarter past ${timeWords[hoursInTwelve]}`;
+    case '30':
+      return `Half past ${timeWords[hoursInTwelve]}`;
+    case '45':
+      return `Quarter to ${timeWords[hoursInTwelve]}`;
+    default:
+      const minutesBounded = minutesWithoutZero > 30 ? 60 - minutes : minutes;
+      const minutesText = getMinutesPluralized(minutesWithoutZero);
+      const transitionWord = minutes > 30 ? ' to ' : ' past ';
+      return `${timeWords[minutesBounded]}${minutesText}${transitionWord}${timeWords[hoursInTwelve]}`;
+  }
+};
 
-/* Listeners to when input boxes are updated to run convertTimeToWords function */
-hour.addEventListener( "input", function (e) 
-{
+const twentyFourToTwelveHour = (hour) => (hour % 12) + 1;
+const removeLeadingZero = (val) => (val == 00 ? '0' : val);
+const getMinutesPluralized = (min) => (min > 1 ? ' minutes ' : ' minute ');
 
-    hour = document.getElementById( 'hour' ).value;      
-    min  = document.getElementById( 'minute' ).value;
-    convertTimeToWords( hour, min );
+const timeWords = {
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+  10: 'ten',
+  11: 'eleven',
+  12: 'twelve',
+  13: 'thirteen',
+  14: 'fourteen',
+  15: 'fifteen',
+  16: 'sixteen',
+  17: 'seventeen',
+  18: 'eighteen',
+  19: 'nineteen',
+  20: 'twenty',
+  21: 'twenty-one',
+  22: 'twenty-two',
+  23: 'twenty-three',
+  24: 'twenty-four',
+  25: 'twenty-five',
+  26: 'twenty-six',
+  27: 'twenty-seven',
+  28: 'twenty-eight',
+  29: 'twenty-nine',
+  30: 'thirty',
+};
 
+hour.addEventListener('input', function (e) {
+  hour = hourField.value;
+  min = minuteField.value;
+  convertTimeToWords(hour, min);
 });
 
-minute.addEventListener( "input", function (e) 
-{
-
-    hour = document.getElementById( 'hour' ).value;      
-    min  = document.getElementById( 'minute' ).value;
-    convertTimeToWords( hour, min );
-
+minute.addEventListener('input', function (e) {
+  hour = hourField.value;
+  min = minuteField.value;
+  convertTimeToWords(hour, min);
 });
 
-minute.addEventListener( "load", function (e) 
-{
-
-    hour = document.getElementById( 'hour' ).value;      
-    min  = document.getElementById( 'minute' ).value;
-    convertTimeToWords( hour, min );
-
+minute.addEventListener('load', function (e) {
+  hour = hourField.value;
+  min = minuteField.value;
+  convertTimeToWords(hour, min);
 });
